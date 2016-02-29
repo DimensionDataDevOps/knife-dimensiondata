5a4d70b3609eb95f45042177734b2937392719ae
Phillip Spies <fspies0@hotmail.com>
2016-02-29 13:29:46 -0500

    Update dimensiondata_template_list.rb

--- lib/chef/knife/dimensiondata_template_list.rb
+++ lib/chef/knife/dimensiondata_template_list.rb
@@ -13,8 +13,8 @@ class Chef::Knife::DimensiondataTemplateList < Chef::Knife::BaseDimensiondataCom
 
   get_common_options
 
-  option :dc,
-         :long => "--datacenter dc_id",
+  option :dimensiondata_dc,
+         :long => "--dimensiondata_dc dc_id",
          :description => "Datacenter id where templates should be retrieved from"
 
   def run
@@ -23,11 +23,11 @@ class Chef::Knife::DimensiondataTemplateList < Chef::Knife::BaseDimensiondataCom
       show_usage
       fatal_exit("You must specify datacenter id for this knife")
     end
-    @platformtemplates = caas.image.template_list_in_location(config[:dc])
+    @platformtemplates = caas.image.template_list_in_location(config[:dimensiondata_dc])
     @platformtemplates.map {| template |
       puts "#{ui.color("Platform Template", :cyan)}: #{ui.color("#{template.id}", :red)} - #{template.name} (#{template.cpu_count} cores,#{template.memory_mb}mb memory)"
     }
-    @customertemplates = caas.image.template_labels_in_location(config[:dc])
+    @customertemplates = caas.image.template_labels_in_location(config[:dimensiondata_dc])
     if (@customertemplates.kind_of?(Array))
     	@customertemplates.map {| template |
       		puts "#{ui.color("Customer Template", :cyan)}: #{ui.color("#{template.id}", :red)} - #{template.name} (#{template.cpu_count} cores,#{template.memory_mb}mb memory)"
